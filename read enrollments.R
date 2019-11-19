2a8436c646fa1e7f22b04e6b29469af63e42f361
brian <brian.c.holt@gmail.com>
2019-11-18 21:59:32 -0800

    added some html to read

--- read enrollments.R
+++ read enrollments.R
@@ -29,7 +29,7 @@ robotstxt:::get_robotstxt('https://inside.seattlecolleges.edu/default.aspx?svc=e
 driver <- rsDriver(browser=c("firefox"))
 remote_driver <- driver[["client"]]
 remote_driver$open()
-
+remote_driver$close()
 remote_driver$navigate("https://inside.seattlecolleges.edu/default.aspx?svc=enrollment&page=enrollment")
 RSelenium:::selKeys %>% names()
 remote_driver$refresh()
@@ -38,12 +38,12 @@ el$highlightElement()
 
 el$clickElement()
 
-el$sendKeysToElement(list('login'))
+el$sendKeysToElement(list('id'))
 
 
 el.1 <- remote_driver$findElement(using = 'xpath', '//*[@id="TxPIN"]')
 el.1$highlightElement()
-el.1$sendKeysToElement(list(''))
+el.1$sendKeysToElement(list('pin'))
 el$sendKeysToElement(list(key='enter'))
 
 col <- c('062','063','064')
@@ -68,6 +68,9 @@ url_item<- paste0("https://inside.seattlecolleges.edu/enrollment/content/display
 remote_driver$navigate(url_item)
 
 page<- remote_driver$getPageSource() %>% .[[1]] %>% read_html()
+
+
+page %>% html_nodes("#rptClusterRpt_ctl417_row") #reads the last tr row, 417
 page_tr<- page %>% html_nodes("tr") %>% html_text()
 
 rows<- page_tr %>% str_squish()
@@ -76,3 +79,58 @@ grep('clus',rows,ignore.case=T)
 ##next, find the "cluster  class" xpath:  /html/body/form/div[3]/span[4]
 
 ##after this,  stip out the headers for each sub table--tricky because 2 rows with different headers.
+
+
+
+
+<td style="text-align: center; border-right: solid 1px #FFFFFF;">
+  9650
+</td>
+  <td style="text-align: left; padding-left: 4px; border-right: solid 1px #FFFFFF; white-space: nowrap;">
+  SSC  101&nbsp;D1</td>
+  <td style="border-right: solid 1px #FFFFFF;" width="*">
+  INTRO TO INFO RES FOR SS</td>
+  <td style="text-align: center; border-right: solid 1px #FFFFFF;">
+  <span id="rptClusterRpt_ctl417_lblCredit">3</span>
+  </td>
+  <td style="border-right: solid 1px #FFFFFF; min-width: 50px;">
+  <span id="rptClusterRpt_ctl417_lbldays" style="font-weight:bold;">Online</span>
+  </td>
+  <td style="border-right: solid 1px #FFFFFF; white-space: nowrap;">
+  <span id="rptClusterRpt_ctl417_lblStartTime"><center>-</center></span>
+  </td>
+  <td style="border-right: solid 1px #FFFFFF; white-space: nowrap;">
+  <span id="rptClusterRpt_ctl417_lblEndTime"><center>-</center></span>
+  </td>
+  <td style="white-space: nowrap; border-right: solid 1px #FFFFFF;">
+  <span id="rptClusterRpt_ctl417_lblRoom"><center>-</center></span>
+  </td>
+  <td style="border-right: solid 1px #FFFFFF;">
+  <span id="rptClusterRpt_ctl417_Instructor">Avillar</span>
+  </td>
+  <td style="text-align: right; width: 20px;">
+  <b>3</b>
+  </td>
+  <td style="text-align: center;">/</td>
+  <td style="text-align: left; width: 20px; border-right: solid 1px #FFFFFF;">
+  <b class="small">18</b>
+  </td>
+  <td style="text-align: center;">
+  <span id="rptClusterRpt_ctl417_lblWaitList">0</span>
+  </td>
+  <td style="text-align: center; border-left: solid 1px #FFFFFF;">
+  <span id="rptClusterRpt_ctl417_FTE">0.6</span>
+  </td>
+  <td style="text-align: center; border-left: solid 1px #FFFFFF;">
+  <span id="rptClusterRpt_ctl417_FTE_state">0.2</span>
+  </td>
+  <td style="text-align: center; border-left: solid 1px #FFFFFF;">
+  <span id="rptClusterRpt_ctl417_PRGindx">011</span></td>
+  <td style="text-align: center; border-left: solid 0px #FFFFFF;">
+  <span id="rptClusterRpt_ctl417_ORGindx">3G05</span></td>
+  <td style="text-align: center; border-left: solid 1px #FFFFFF;">
+  <span id="rptClusterRpt_ctl417_adminUnit">KK</span></td>
+  <td style="text-align: center; border-left: solid 1px #FFFFFF;">
+  <span id="rptClusterRpt_ctl417_Cluster">9650B903</span>
+  </td>
+  
