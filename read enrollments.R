7ed26bbac079fcfd2dc36806c92a6c82da2bebce
brian <brian.c.holt@gmail.com>
2019-12-31 16:11:37 -0800

    reorg script to be clear

--- read enrollments.R
+++ read enrollments.R
@@ -1,32 +1,15 @@
 library(RSelenium)
 library(rvest)
 library(tidyverse)
-col <- c('062','063','064')
-q <- c('B903')
-qn <- c('Winter 20')
-
-url.1 <- "https://inside.seattlecolleges.edu/default.aspx?svc=enrollment&page=enrollment"
-
-content <- read_html(url.1)
- content %>%  html_text(.,trim = F) %>%
-  jsonlite::fromJSON(.)
 
 
+#Using selenium
+# sign in to inside seattle colleges --------------------------------------
 
-
-download.file(url.1, destfile = "scrapedpage.html", quiet=TRUE)
-content <- read_html("scrapedpage.html")
-content %>% html_nodes(xpath='/html/head/title')
-content %>%  html_table(.,fill=T) -> table.list
-content %>% html_nodes("tr")
-
-
-#selenium
-robotstxt:::get_robotstxt('https://inside.seattlecolleges.edu/default.aspx?svc=enrollment&page=enrollment')
 driver <- rsDriver(browser =c("firefox"))
 ##https://github.com/ropensci/RSelenium/issues/116
 remote_driver <- driver[["client"]]
-#remote_driver$open()
+#remote_driver$close()
 
 remote_driver$navigate("https://inside.seattlecolleges.edu/default.aspx?svc=enrollment&page=enrollment")
 RSelenium:::selKeys %>% names()
@@ -45,6 +28,13 @@ el.1$highlightElement()
 el.1$sendKeysToElement(list('id'))
 el$sendKeysToElement(list(key='enter'))
 
+
+# 1 select college , quarter and year navigate to that page
+
+col <- c('062','063','064') # central north south
+q <- c('B903')  #After 2010, 2019 verging on 2020, quarter: winter "3"
+qn <- c('Winter 20')
+
 col <- c('062','063','064')
 q <- c('B903')
 qnum <- c(1:4)
@@ -61,10 +51,6 @@ year.quarter
 cbind(year.quarter)
 
 
-
-
-read_html(url.1)
-
 ##selecting college
 
 pick_col<- remote_driver$findElement(using = 'xpath', '//*[@id="ctl08_ddlCollegeView"]')
@@ -75,20 +61,49 @@ pick_qu$clickElement()
 #A782  = =  07 - 08 years, 2 =  fall.  If you want after 2010, must use B782, which would be fall 2017-18
 ##
 
-url <- "https://inside.seattlecolleges.edu/enrollment/content/displayReport.aspx?col=063&q=A012&qn=Fall 01&nc=false&in=&cr="
+# The following beings with fall 2006 at North, noted by these  --------
+# col=063&q=A012&qn=Fall 01
+
+url <- "https://inside.seattlecolleges.edu/enrollment/content/displayReport.aspx?col=063&q=A672&qn=Fall 06&nc=false&in=&cr="
+
+
+#
+# 1 scrape full page
+
 remote_driver$navigate(url)
 
 page<- remote_driver$getPageSource() %>% .[[1]] %>% read_html()
 
+
+
+
+# 1a scrape site's campus and quarter/year
+#
+# 2. scrape and tidy generated eroll count
+# get site generated count for all non clusterd classes: ------------------
+
+xpath.from.src <- '//*[@id="lblEnrollNon"]'  #xpath for total from the bottom of site page
+page %>% html_nodes(.,xpath=xpath.from.src)->node.out
+site.enrolled.count<- html_text (node.out)
+site.enrolled.count<- as.numeric(sub(",","",site.enrolled.count))
+
+site.enrolled.count
+
+#
+# 3. scrape and tidy main table
+#
 xpath.from.src <- "/html/body/form/div[3]/table[1]" #xpath for non-clusterd classes
 page %>% html_nodes(.,xpath=xpath.from.src)->table.out
 
 table.out %>% html_table(fill = T)-> enroll.report
 
-#url_item<- paste0("https://inside.seattlecolleges.edu/enrollment/content/displayReport.aspx?col=",col[2],"&q=",q,"&qn=",qn,"&nc=false&in=&cr=")
+### the following just saves a single enroll.report for practice in tidying
 remote_driver$navigate(url.1)
 
 save(enroll.report,file='enroll_report.RData')
+
+
+
 
 
 ##after this,  stip out the headers for each sub table--tricky because 2 rows with different headers.
@@ -102,7 +117,7 @@ remote_driver$close()
 
 # Load and Clean ----------------------------------------------------------
 
-load("enroll_report.RData")
+#load("enroll_report.RData")
 d<- enroll.report[[1]]
 
 # Get and correct column names --------------------------------------------
@@ -136,7 +151,8 @@ col.names <- colnames(d[,c(1:3,5,8:9,15:18)])
 d[col.names] <- lapply(d[col.names],factor)
 str(d)
 
-# some checks with summary output from the URL ----------------------------
+# 4. count enrollments
+
 
 d %>% mutate(cancel=ifelse(Instructor == "Cancelled",1,0)) %>% group_by(cancel) %>% summarize(sumEnrol=sum(Enrolled),sumFTE=sum(Total.FTES),sumStateFte=sum(State.FTES),meanEnr=sumEnrol/n())
 
@@ -147,5 +163,27 @@ stem(d$Class.Size)
 
 d %>% mutate(cancel=ifelse(Instructor == "Cancelled",1,0)) %>% group_by(cancel) %>% ggplot(aes(x=Class.Size)) + geom_histogram()+facet_wrap(~cancel)
 # Histograms of class sizes -----------------------------------------------
+
+
+
+
+
+#
+# 5. compare site vs calculated enrollment
+#
+sum(d$Enrolled)-site.enrolled.count
+
+# 6. store data into following fields:
+#
+#   campus
+# site quarter
+# site year
+# site enrollment
+# scraped enrollment
+# difference between site and scraped enrollment
+
+
+
+
 
 
